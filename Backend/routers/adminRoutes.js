21113e8dec1a9376d094cb62b5445d2167d954a4
nilunisandunika03 <niluniniralgama2022@gmail.com>
2026-07-11 04:08:50 +0530

    doctor verification completed to system to be deployable

--- Backend/routers/adminRoutes.js
+++ Backend/routers/adminRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getSystemStats, getAllUsers, updateUserRole, deleteUser, getPendingRequests, approveRequest, rejectRequest, addUser, updateUserDetails, updateUserStatus } from '../controllers/adminController.js';
+import { getSystemStats, getAllUsers, updateUserRole, deleteUser, getPendingRequests, approveRequest, rejectRequest, requestAdditionalDocuments, addUser, updateUserDetails, updateUserStatus } from '../controllers/adminController.js';
 import { protect } from '../middlewares/authMiddleware.js';
 import { adminOnly } from '../middlewares/roleMiddleware.js';
 
@@ -24,5 +24,6 @@ router.delete('/users/:id', deleteUser);
 router.get('/requests', getPendingRequests);
 router.put('/requests/:id/approve', approveRequest);
 router.put('/requests/:id/reject', rejectRequest);
+router.put('/requests/:id/request-docs', requestAdditionalDocuments);
 
 export default router;
